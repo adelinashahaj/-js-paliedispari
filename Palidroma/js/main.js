809a83ae38ef5ba9bfda9d235a221ba055c71c65
adelinashahaj <adelinarucaj@icloud.com>
2023-03-02 16:25:22 +0100

    ho cerato una stringa

--- Palidroma/js/main.js
+++ Palidroma/js/main.js
@@ -1,4 +1,11 @@
 
-//chiedo all utente di inserire un ararola
+//chiedo all utente di inserire un parola
 
 let parolaDom = prompt("Inserisci una parola");
+console.log(parolaDom);
+
+const stringa = "RADAR";
+
+for (let i = 0; i < stringa.length; i++){
+    console.log(stringa[i]);
+}
